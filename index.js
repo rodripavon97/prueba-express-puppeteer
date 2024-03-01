77b4dbeb7f1800e2cbd9ade41e1c2f11713800a7
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2024-02-29 23:29:29 -0300

    first changes

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.get('/pdf', async (req, res) => {
+  const url = req.query.url;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url, {
+    waitUntil: 'networkidle0',
+  });
+
+  const pdfBuffer = await page.pdf({
+    format: 'A4',
+    margin: {
+      top: '1cm',
+      right: '1cm',
+      bottom: '1cm',
+      left: '1cm',
+    },
+  });
+
+  await browser.close();
+
+  res.setHeader('Content-Type', 'application/pdf');
+  res.send(pdfBuffer);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
